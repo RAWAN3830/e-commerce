9a03118f8bae4229a28a5bd934c1fb2c427b8739
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2024-09-18 15:28:27 +0530

    first commit

--- lib/core/services/cart_services/recive_to_cart.dart
+++ lib/core/services/cart_services/recive_to_cart.dart
@@ -11,9 +11,7 @@ Future<List<ProductModel>> getCartProducts() async {
   final firestore = FirebaseFirestore.instance;
   final uid = FirebaseAuth.instance.currentUser?.uid;
   cartProducts = [];
-  final data = await firestore.collection(FirestoreCollections.cart)
-      .doc(uid)
-      .get();
+  final data = await firestore.collection(FirestoreCollections.cart).doc(uid).get();
   final productList = await data.data()?['products'];
   for (var product in productList) {
     cartProducts.add(ProductModel.fromJson(product));
